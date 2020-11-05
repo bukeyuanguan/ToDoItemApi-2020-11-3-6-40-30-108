54584681ed5f0a883ff6be7d7104c9aaf2f29234
Ge Luxue <lge5@slb.com>
2020-11-05 12:38:35 +0800

    Commit Backen Api

---
+++ ToDoItemApi/Controllers/ToDoItemController.cs
@@ -0,0 +1,29 @@
+﻿using ToDoItems;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace ToDoItemApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoItemsController : ControllerBase
+    {
+        private MemoryRepository _repository = MemoryRepository.Instance;
+
+        [HttpGet]
+        public async Task<ActionResult<List<ToDoItem>>> QueryAsync()
+        {
+            var models = await _repository.QueryAsync();
+            return Ok(models);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<ToDoItem>>> GetAsync(
+            long id)
+        {
+            var list = await _repository.GetAsync(id);
+            return Ok(list);
+        }
+    }
+}
